2d33e4c585a25f8fe2552f385f3cde02aae67cdd
Shahabr99 <sean.rohani9@gmail.com>
2023-07-13 16:02:52 -0700

    adding routes

--- db.js
+++ db.js
@@ -5,9 +5,9 @@ const { Client } = require('pg')
 let DB_URI;
 
 if(process.env.ENV_NODE === 'test') {
-  DB_URI = "postgresql:///biztime_test";
+  DB_URI = "postgresql://postgres:myPassword@localhost:5432/biztime_test";
 }else{
-  DB_URI = "postgresql:///biztime";
+  DB_URI = "postgresql://postgres:myPassword@localhost:5432/biztime";
 }
 
 
--- routes/companies.js
+++ routes/companies.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const router = new express.Router();
+const ExpressError = require('../expressError')
 const db = require('../db')
 
 
-router.get('/companies', async function(req, res, next) {
+router.get('/', async function(req, res, next) {
   try {
     const results = await db.query('SELECT code, name FROM companies');
     return res.json({companies: results.rows})
   }catch(e){
+    return next(e)
+  }
+})
+
+
+router.get('/:code', async function(req, res, next) {
+  try{
+    const {code} = req.params
+    const results = await db.query("SELECT * FROM companies WHERE code=$1", [code])
+    return res.json(results.rows[0])
+  }catch(e) {
     return next(e)
   }
 })
